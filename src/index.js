f3b56ea74569a97e137803d40fb8137473e07f6c
Robert King <Harold.R.King@aexp.com>
2018-09-11 16:51:30 -0700

    chore(maintain)
    Add license, package, and export index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @module roking-a11y
+ * @author H Robert King <hrobertking@cathmhaol.com>
+ * @description Accessibility evaluation tools
+ */
+
+var readability = require('./readability.js');
+exports.readability = readability.default;
+
