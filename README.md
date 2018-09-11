b1780bfd0c2b6ae5572b9556fa8e7ab79ea123a3
Robert King <Harold.R.King@aexp.com>
2018-09-11 16:47:55 -0700

    feature(Readability)
    Add Readability

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # roking-a11y
 Accessibility tools
+
+## Readability
+The Readability utility calculates the Läsbarhetsindex for content.
+
+### Constructor
+```
+new Readability([**_string|string[]|HTMLElement_ sample**][, **_number_ wlen**])
+```
+
+- sample:
+- wlen: length of a longword, defaults to 6 characters
+
+### Internal Objects
+- @typedef {Object} ScoredItem
+- @description A scored phrase
+- @property {number} longwords
+- @property {string} phrase
+- @property {number} score
+- @property {number} sentences
+- @property {number} words
+
+### Properties
+- **_number_ avg**: The average Läsbarhetsindex for all parsed content. READ-ONLY
+- **_string|string[]|HTMLElement_ content**: Raw content to be evaluated.
+- **_string_ lang**: BCP-47 langtag for content.
+- **_ScoredItem[]_ parsed**: Phrases parsed into scored items, set by setting content or passing content to the `score` method. READ-ONLY
+- **number_ wlong**: The length, in characters, a word must be to be a long word. Defaults to 6 characters.
+
+### Methods
+- **_ScoredItem_ item(_number_ index)**: Returns a scored item from the `parsed` collection.
+- **_number_ score([_string|string[]|HTMLElement_ content])**: Calculates and returns the Läsbarhetsindex for the specified content.
+
--- src/readability.js
+++ src/readability.js
@@ -2,99 +2,57 @@
  * @module roking-a11y
  * @author H Robert King <hrobertking@cathmhaol.com>
  * @description The Readability utility calculates the Läsbarhetsindex for content.
- * @param {string|string[]|HTMLElement} content - content to evaluate
+ * @param {string|string[]|HTMLElement} sample - content to evaluate
  * @param {number} wlen - length of a longword, defaults to 6 characters
+ *
+ * @typedef {Object} ScoredItem
+ * @description A scored phrase
+ * @property {number} longwords
+ * @property {string} phrase
+ * @property {number} score
+ * @property {number} sentences
+ * @property {number} words
+ *
  */
-export default class Readability {
-  constructor(content, wlen) {
-    this.wlong = wlen || 6;
-    this.content = content;
-  }
-
+exports.default = function Readability(sample, wlen) {
   /**
    * @property avg
    * @type {number}
    * @description The average Läsbarhetsindex for parsed content.
    * @read-only
    */
-  get avg() {
-    if (this.parsed) {
-      const sum = this.rawScores.reduce((ttl, num) => Number(num || 0) + Number(ttl || 0));
-      return sum/this.rawScores.length;
-    }
-  }
+	Object.defineProperty(this, 'avg', {
+  	get: getAvg,
+  	enumerable: true,
+	});
 
   /**
    * @property content
    * @type {string|string[]|HTMLElement}
    * @description Raw content to be evaluated.
    */
-  set content(data) {
-    const isPrimitive = value => /^(boolean|number|string)$/.test(typeof value);
-    const isElement = value => value && value.hasChildNodes;
-    const isDocument = value => value && value.body;
-
-    /* returns a string array given a primitive value or htmlelement */
-    const normalize = (phrase) => {
-      const els = [];
-      const tags = isElement ? (phrase.body || phrase).getElementsByTagName('*') : null;
-      if (isPrimitive(phrase)) {
-        return [`${phrase}`];
-      }
-      if (tags) {
-        for (let c = tags.length - 1; c > -1; c -= 1) {
-          els.unshift(tags.item(c).textContent.replace(/\s+/, ' '));
-        }
-        return els;
-      }
-      return els[];
-    };
-
-    /* returns an object with word, longword, & sentence counts with score */
-    const parse = (phrase) => {
-      const bites = phrase.split(' ');
-      const words = bites.length;
-      const longWords = bites.filter(word => word.length >= this.wlong).length;
-      const sentences = phrase.split(/[:.]/g).length;
-      const score = !words || !sentences ? 0 : Math.round(words/sentences) + ((longWords*100)/words);
-
-      return {
-        longWords,
-        phrase,
-        score,
-        sentences,
-        words,
-      };
-    }
-
-    this.parsed = Array.isArray(data) ?
-      data.map(el => parse(el)) :
-      normalize(data).map(el => parse(el));
-  }
+  Object.defineProperty(this, 'content', {
+    set: setContent,
+    enumerable: true,
+    writeable: true,
+  });
 
   /**
    * @property lang
    * @type {string}
    * @description BCP-47 langtag for content.
    */
-  set lang(bcp47) {
-    // TODO: override wlong for specified language
-  }
+  Object.defineProperty(this, 'lang', {
+    set: setLang,
+    enumerable: true,
+    writeable: true,
+  });
 
-  /**
-   * @typedef {Object} ScoredItem
-   * @description A scored phrase
-   * @property {number} longwords
-   * @property {string} phrase
-   * @property {number} score
-   * @property {number} sentences
-   * @property {number} words
-   */
-  
   /**
    * @property parsed
    * @type {ScoredItem[]}
-   * @description Phrases parsed into scored items.
+   * @description Phrases parsed into scored items. Set by setting content or passing content
+   * to the `score` method.
    */
 
   /**
@@ -103,6 +61,7 @@ export default class Readability {
    * @description The length, in characters, a word must be to be a long word.
    * @default 6
    */
+  this.wlong = wlen || 6;
 
   /**
    * @method item
@@ -110,9 +69,9 @@ export default class Readability {
    * @returns {ScoredItem}
    * @param {number} index
    */
-  item(index) {
-    return this.parsed ? this.parsed[index] : null;
-  }
+  this.item = function getItem(index) {
+    return self.parsed ? self.parsed[index] : null;
+  };
 
   /**
    * @method score
@@ -120,12 +79,126 @@ export default class Readability {
    * @returns {number[]}
    * @param {string[]} content
    */
-  score(content) {
+  this.score = function calculate(content) {
     /* parse the content if it's passed in */
     if (content) {
-      this.content = content;
+      self.content = content;
     }
 
-    return this;
+    return self;
   }
+
+  /**
+   * @private
+   * @description Returns the average score for all parsed content
+   * @returns {number}
+   */
+  function getAvg() {
+    if (self.parsed && self.parsed.length) {
+      const scores = self.parsed.map(function mapScoredItem(si) {
+          return si.score;
+        });
+      const sum = scores.reduce(function accumulate(ttl, score) {
+          Number(score || 0) + Number(ttl || 0);
+        });
+      return sum/self.parsed.length;
+    }
+  }
+
+  /**
+   * @private
+   * @description Returns true if the type is primitive
+   * @returns {boolean}
+   * @param {*} value
+   */
+  function isPrimitive(value) {
+    return /^(boolean|number|string)$/.test(typeof value);
+  }
+
+  /**
+   * @private
+   * @description Returns true if the object is an element with child nodes
+   * @returns {boolean}
+   * @param {*} value
+   */
+  function isElement(value) {
+    return value && value.hasChildNodes;
+  }
+
+  /**
+   * @private
+   * @description Returns true if the type has a body
+   * @returns {boolean}
+   * @param {*} value
+   */
+  function isDocument(value) {
+    return value && value.body;
+  }
+
+  /**
+   * @private
+   * @description Returns a string array given a primitive value or htmlelement
+   * @returns {string[]}
+   * @param {*} phrase
+   */
+  function normalize(phrase) {
+    const els = [];
+    const tags = isElement(phrase) ? (phrase.body || phrase).getElementsByTagName('*') : null;
+
+    if (isPrimitive(phrase)) {
+      els.push(phrase + '');
+    } else if (tags) {
+      for (let c = tags.length - 1; c > -1; c -= 1) {
+        els.unshift(tags.item(c).textContent.replace(/\s+/, ' '));
+      }
+    }
+    return els;
+  }
+
+  /**
+   * @private
+   * @description returns an object with word, longword, & sentence counts with score
+   * @returns {object}
+   * @param {string} phrase
+   */
+  function parse(phrase) {
+    const bites = phrase.split(' ');
+    const words = bites.length;
+    const longWords = bites.filter(function countlong(word) { return word.length >= self.wlong; }).length;
+    const sentences = phrase.split(/[:.]/g).length;
+    const score = !words || !sentences ? 0 : Math.round(words/sentences + (longWords*100)/words);
+
+    return {
+      longWords,
+      phrase,
+      score,
+      sentences,
+      words,
+    };
+  }
+
+  /**
+   * @private
+   * @description Sets the parsed property using the provided content
+   * @returns {undefined}
+   */
+  function setContent(data) {
+    self.parsed = Array.isArray(data) ?
+      data.map(el => parse(el)) :
+      normalize(data).map(el => parse(el));
+  }
+
+  /**
+   * @private
+   * @description Sets the wlong property based on the specified language 
+   * @returns {undefined}
+   */
+  function setLang(bcp47) {
+    // TODO: override self.wlong default value based on specified language
+  }
+
+  const self = this;
+
+  // constructor
+  this.content = sample;
 }
